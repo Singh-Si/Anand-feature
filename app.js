fa591d977b4962c1b50589b4a8514c3878c8d02d
singh-Si <anandchauhan15798@gmail.com>
2023-07-16 20:20:23 +0530

    frist commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+require("./config/congig")
+
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.json("hell Anand")
+})
+
+app.listen(4040,()=>{
+    console.log("server is started at prot 4040")
+})
